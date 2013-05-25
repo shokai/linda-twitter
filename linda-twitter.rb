1c822b84144b8beca5eb6ea8b3cc1e6a78ccf394
Sho Hashimoto <hashimoto@shokai.org>
2013-05-25 23:24:59 +0900

    tweet with Linda

---
+++ linda-twitter.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'tw'
+require 'sinatra/rocketio/linda/client'
+$stdout.sync = true
+
+url   = ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
+space = ENV["LINDA_SPACE"] || "test"
+user  = ENV["TW_USER"]
+puts "connecting.. #{url}"
+linda = Sinatra::RocketIO::Linda::Client.new url
+ts = linda.tuplespace[space]
+
+puts "auth twitter (#{user})"
+tw = Tw::Client.new
+tw.auth(user)
+
+linda.io.on :connect do
+  puts "connect!! <#{linda.io.session}> (#{linda.io.type})"
+  ts.watch ["twitter", "tweet"] do |tuple|
+    p tuple
+    if tuple.size == 3 and !["success", "fail"].include?(tuple[2])
+      begin
+        tw.tweet tuple[2]
+        ts.write ["twitter", "tweet", "success"]
+      rescue
+        ts.write ["twitter", "tweet", "fail"]
+      end
+    end
+  end
+end
+
+linda.io.on :disconnect do
+  puts "RocketIO disconnected.."
+end
+
+linda.wait
